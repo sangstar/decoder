82a9d437b94758297b743dd3f0065dfa41a69aaf
Sanger Steel <sangersteel@Sangers-MacBook-Air.local>
2023-08-01 16:06:25 -0400

    initial commit

---
+++ utils.py
@@ -0,0 +1,14 @@
+import numpy as np 
+
+def load_glove_embeddings(file_path):
+    embeddings = {}
+    with open(file_path, 'r', encoding='utf-8') as f:
+        for line in f:
+            values = line.split()
+            word = values[0]
+            vector = np.array(values[1:], dtype='float32')
+            embeddings[word] = vector
+    return embeddings
+
+# Usage
+glove_embeddings = load_glove_embeddings('glove.6B.100d.txt')
